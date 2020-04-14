2f70d6ae54824e3813780e7960bc7a64ed55f8c4
pti4life <kallai.roland@outlook.com>
2020-04-14 12:38:51 +0200

    initial commit

---
+++ Car-mechanic/Car-mechanic/DataProviders/WorkDataProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Car_mechanic.Modells;
+using Newtonsoft.Json;
+
+namespace Car_mechanic.DataProviders
+{
+    class WorkDataProvider
+    {
+        private static string _URL = "http://localhost:1154/api/work";
+
+        public static IList<Work> GetAll()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(_URL).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    var works = JsonConvert.DeserializeObject<List<Work>>(rawData);
+                    return works;
+                }
+                else
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void UpdateWork(Work work)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(work);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PutAsync(_URL, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Car-mechanic/Car-mechanic/WorkWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Car_mechanic.DataProviders;
+using Car_mechanic.Modells;
+
+namespace Car_mechanic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class WorkWindow : Window
+    {
+        private Work _work;
+        public WorkWindow(Work work)
+        {
+            InitializeComponent();
+
+            _work = work;
+            DetailOfIssues.Content = _work.DetailOfIssues;
+            CarLicensePlate.Content = _work.CarLicensePlate;
+            StateOfWork.Text = _work.StateOfWork;
+
+        }
+
+        private void ModifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            _work.StateOfWork = StateOfWork.Text;
+            WorkDataProvider.UpdateWork(_work);
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
